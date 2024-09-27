62f471361e84718bce26369241cf1dee9a334048
Davi Luiz <aloha.devdl@gmail.com>
2024-09-26 22:03:17 -0400

    tooltip

--- JS/Script.js
+++ JS/Script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from'./module/scrollanimado.js';
 import initAccordion from './module/accordion.js';
 import initTabMenu from './module/tabmenu.js';
 import initModal from './module/modal.js';
+import initTooltip from './module/tooltip.js';
 
 initTabMenu();
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initModal();
+initTooltip();
