50e787822ddbb2e9f524ebe700e4413ed86aab5b
Davi Luiz <aloha.devdl@gmail.com>
2024-10-11 12:54:43 -0400

    dropdown
    modulo de dropdown do menu

--- JS/module/dropdown.js
+++ JS/module/dropdown.js
@@ -1,12 +1,23 @@
+import outsideClick from './outsideclick.js';
+
+
 export default function initDropdown(){
     const drop = document.querySelectorAll('[data-dropdown]');
 
     drop.forEach(menu =>{//para adicionar dois ou mais eventos posso criar uma array ['touchstart', 'click'].forEach(userEvent => { menu.addEventListener(userEvent, handleClick)}) ambos funcionam
-        menu.addEventListener('click', handleClick) 
+        ['touchstart', 'click'].forEach(userEvent => { 
+            menu.addEventListener(userEvent, handleClick)
+        })
+        //para adcionar apenas um evento =>   menu.addEventListener('click', handleClick) 
     })
 
     function handleClick(event){
         event.preventDefault();
-        this.classList.toggle('active')
-    }
+        this.classList.add('active')
+        outsideClick(this, ['touchstart', 'click'], ()=>{
+            this.classList.remove('active');
+        });
+    };
+
+    
 }
