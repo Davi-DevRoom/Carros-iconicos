d33f93a15e480364374ad8c70ab9654b11239652
Davi Luiz <aloha.devdl@gmail.com>
2025-01-06 11:30:33 -0400

    Update JS
    Adaptação de cores clicando nas imagens dos carros

--- JS/module/funcionamento.js
+++ JS/module/funcionamento.js
@@ -6,11 +6,7 @@ const funciona = document.querySelector('[data-semana]');
 const diasSemana = funciona.dataset.semana.split(',').map(Number);
 const horarioSemana = funciona.dataset.horario.split(',').map(Number);
 
-
-console.log(horarioSemana);
-
 const dataAgora = new Date();
 const diaAgora = dataAgora.getDay();
 const horarioAgora = dataAgora.getHours();
 
-console.log(horarioAgora);
